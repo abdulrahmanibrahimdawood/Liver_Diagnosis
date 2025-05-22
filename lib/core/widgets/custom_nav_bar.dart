7dea4acdbda4543b12b62beade077ea40f063a5f
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-05-22 15:58:03 +0300

    fix worning

--- lib/core/widgets/custom_nav_bar.dart
+++ lib/core/widgets/custom_nav_bar.dart
@@ -31,65 +31,6 @@ class _MainPageState extends State<CustomNavBar> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: _pages,
-        ),
-        bottomNavigationBar: Container(
-          height: 78.h,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          child: BottomNavigationBar(
-            selectedLabelStyle:
-                TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp),
-            unselectedLabelStyle: TextStyle(fontSize: 12.sp),
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            items: List.generate(4, (index) {
-              return BottomNavigationBarItem(
-                icon: Container(
-                  padding: EdgeInsets.all(8.sp),
-                  decoration: BoxDecoration(
-                    color: _selectedIndex == index
-                        ? AppColor.kPrimaryColor.withValues(alpha: .2)
-                        : Colors.transparent,
-                    borderRadius: BorderRadius.circular(12.r),
-                  ),
-                  child: SvgPicture.asset(
-                    _selectedIndex == index
-                        ? _activeIcons[index]
-                        : _inactiveIcons[index],
-                    width: 22.sp,
-                    height: 22.sp,
-                  ),
-                ),
-                label: index == 0
-                    ? 'Home'
-                    : index == 1
-                        ? 'Labs'
-                        : index == 2
-                            ? 'Qs & Ans'
-                            : 'Doctors',
-              );
-            }),
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: AppColor.kPrimaryColor,
-            unselectedItemColor: Colors.grey,
-            selectedFontSize: 12.sp,
-            unselectedFontSize: 12.sp,
-            elevation: 0,
-          ),
-        ),
-      ),
-    );
-  }
-
   final List<String> _activeIcons = [
     'assets/images/home_active.svg',
     'assets/images/medical_analysis_active.svg',
@@ -103,4 +44,67 @@ class _MainPageState extends State<CustomNavBar> {
     'assets/images/QandAns.svg',
     'assets/images/doc.svg',
   ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
+
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _pages,
+        ),
+        bottomNavigationBar: isKeyboardOpen
+            ? const SizedBox.shrink()
+            : Container(
+                height: 78.h,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: BottomNavigationBar(
+                  selectedLabelStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp),
+                  unselectedLabelStyle: TextStyle(fontSize: 12.sp),
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTapped,
+                  items: List.generate(4, (index) {
+                    return BottomNavigationBarItem(
+                      icon: Container(
+                        padding: EdgeInsets.all(8.sp),
+                        decoration: BoxDecoration(
+                          color: _selectedIndex == index
+                              ? AppColor.kPrimaryColor.withValues(alpha: .2)
+                              : Colors.transparent,
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                        child: SvgPicture.asset(
+                          _selectedIndex == index
+                              ? _activeIcons[index]
+                              : _inactiveIcons[index],
+                          width: 22.sp,
+                          height: 22.sp,
+                        ),
+                      ),
+                      label: index == 0
+                          ? 'Home'
+                          : index == 1
+                              ? 'Labs'
+                              : index == 2
+                                  ? 'Qs & Ans'
+                                  : 'Doctors',
+                    );
+                  }),
+                  type: BottomNavigationBarType.fixed,
+                  selectedItemColor: AppColor.kPrimaryColor,
+                  unselectedItemColor: Colors.grey,
+                  selectedFontSize: 12.sp,
+                  unselectedFontSize: 12.sp,
+                  elevation: 0,
+                ),
+              ),
+      ),
+    );
+  }
 }

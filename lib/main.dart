583f04264fcc31adc96d4b3a3ecf9b7899bcb62b
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-10-18 16:52:59 +0300

    remove localizationsDelegates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:liver_diagnosis/core/helper_functions/on_generate_routes.dart';
 import 'package:liver_diagnosis/core/services/custom_bloc_observer.dart';
@@ -9,7 +8,6 @@ import 'package:liver_diagnosis/core/services/get_it_servece.dart';
 import 'package:liver_diagnosis/core/services/shared_preferences_singleton.dart';
 import 'package:liver_diagnosis/features/splash/presentation/views/splash_view.dart';
 import 'package:liver_diagnosis/firebase_options.dart';
-import 'package:liver_diagnosis/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,12 +37,6 @@ class HebatoDiagnosis extends StatelessWidget {
           theme: ThemeData(
             fontFamily: 'Cairo',
           ),
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
           debugShowCheckedModeBanner: false,
           onGenerateRoute: onGenerateRoutes,
           home: const SplashView(),
